fda72d5d5febb14d74c43ef08293fa42d6435289
James Jaramillo <jjaram53@uniminuto.edu.co>
2026-07-08 20:59:35 -0500

    Se limpia el init

--- modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import inicio
-from . import registro
-from . import estado_pruebas
-from . import programacion
-from . import evaluacion
-from . import dashboard
-from . import gestion_docentes, gestion_estudiantes, vista_maestra, registro
